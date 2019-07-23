fac1eb06520cc561d8f1c20ec5323e095eacd4dd
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-07-23 14:51:14 -0300

    Utilizando yup para validar os dados recebidos no signin e signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatorio'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caractéres')
+    .required('A senha é obrigatória.'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -13,7 +24,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="name" type="text" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <Input name="password" type="password" placeholder="Sua senha" />
